8bacc6458b2d49cdb205c19213b1079a13be2b92
Jeff McCommas <jeffmccommas@gmail.com>
2015-05-11 07:07:06 -0400

    initial commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,80 @@
-'use strict';
+(function () {
+    "use strict";
+    angular
+        .module('app', [
+            'ngAnimate',
+            'ngResource',
+            'ngMessages'
 
-/**
- * @ngdoc overview
- * @name instagramSearcherApp
- * @description
- * # instagramSearcherApp
- *
- * Main module of the application.
- */
-angular
-  .module('instagramSearchApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ui.router'
+        ]).controller('InstagramController', function(InstagramService, $sce, $window){
+            var vm = this;
+            vm.keyword = '';
+            vm.display = false;
+            vm.count = 0;
+            vm.data = null;
+            vm.message = '';
 
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+            vm.trustSrc = function (src) {
+                return $sce.trustAsResourceUrl(src);
+            };
+            function ErrorHandling() {
+                vm.message = 'Oops something went wrong ,please try again';
+                vm.data = null;
+                vm.display = true;
+            }
+
+            vm.submit = function () {
+                vm.message = 'Please wait while we get your results';
+                if (!isBlank(vm.keyword)) {
+                    InstagramService.get(vm.keyword).then(function (result) {
+                        vm.data = result.data.data;
+                        vm.count = result.data.data.length;
+                        vm.display = true;
+                        vm.message = 'We found ' + vm.count + ' results for ' + vm.keyword;
+                    }).catch(function (error) {
+                        ErrorHandling();
+                    })
+                }
+                else {
+                    ErrorHandling();
+                }
+            };
+            vm.keywordOnChange = function () {
+                if (vm.keyword === undefined) {
+                    vm.display = false;
+                }
+            };
+            function isBlank(str) {
+                return (!str || /^\s*$/.test(str));
+            }
+            vm.openLinkWindow = function (link) {
+                $window.open(link);
+            };
+
+
+        }).service('InstagramService', function ($http) {
+            var result = null;
+            return {
+                'get': function (keyword) {
+                    var clientId = 'bd3de9a0d90f41e8847fa08e838b4a64';
+                    var url = 'https://api.instagram.com/v1/tags/' + keyword + '/media/recent';
+                    var request = {
+                        callback: "JSON_CALLBACK",
+                        client_id: clientId
+                    };
+
+                    return $http({
+                        method: 'JSONP',
+                        url: url,
+                        params: request
+                    }).success(function (response) {
+                        result = response;
+                    }).error(function (error) {
+                        result = error;
+                    });
+                }
+            }
+
+        });
+}());
+
